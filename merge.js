925baf8303cb8378e06b54609855f799b902b5d5
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-10 13:42:22 -0400

    wrote the initial mergeSort function

--- merge.js
+++ merge.js
@@ -1,1 +1,9 @@
-console.log("I am merge");
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return merge(left, right);
+}
