1b03a645fc944f32746c455e116985a71ef19e8f
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-10 13:35:20 -0400

    wrote the fibs function using iteration to get the fibonacci sequence.

--- fibs.js
+++ fibs.js
@@ -1,1 +1,11 @@
-console.log("I am fibs");
+function fibs(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result.slice(0, n);
+}
+
+console.log(fibs(8));
