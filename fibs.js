5506a148fdc033d62667de275fcaf94d1b0843eb
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-10 13:39:46 -0400

    Wrote the fibsRec function to solve the same problem recursively

--- fibs.js
+++ fibs.js
@@ -9,3 +9,15 @@ function fibs(n) {
 }
 
 console.log(fibs(8));
+
+function fibsRec(n) {
+  console.log("This was printed recursively");
+
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+  return [...prev, prev[prev.length - 1] + prev[prev.length - 2]];
+}
+
+console.log(fibsRec(8));
